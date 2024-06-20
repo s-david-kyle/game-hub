65b67f224914dd98a374111503e27c6d56897b2f
S. David Kyle <sdavidkyle@gmail.com>
2024-06-19 17:22:20 -0700

    chore: Update App component layout using Chakra Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem gridArea="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show>
+        <GridItem gridArea="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem gridArea="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
