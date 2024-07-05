8026f767e5be1b305e73596bdfffebef7609746e
S. David Kyle <sdavidkyle@gmail.com>
2024-07-05 15:41:42 -0700

    refactor: Add PlatformSelector component and use it in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid.tsx";
 import GenreList from "./components/GenreList.tsx";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres.ts";
+import PlatformSelector from "./components/PlatformSelector.tsx";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -31,6 +32,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem gridArea="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
